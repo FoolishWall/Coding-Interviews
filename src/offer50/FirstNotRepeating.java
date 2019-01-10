e84a1d5a553c632cd143dad25af3e766eb101174
wall <798657709@qq.com>
2019-01-10 10:38:29 +0800

    第一个只出现一次的字符

--- src/offer50/FirstNotRepeating.java
+++ src/offer50/FirstNotRepeating.java
@@ -3,7 +3,7 @@ package offer50;
 
 import java.util.*;
 
-/**git
+/**
  * @author wall
  * @date 2019/1/7  16:48
  * @description 第一个只出现一次的字符
@@ -12,8 +12,10 @@ import java.util.*;
 public class FirstNotRepeating {
     //测试
     public static void main(String[] args) {
-        String string = "abaccdeff";
-        System.out.println(firstNotRepeating_mysolution(string));
+        String string = "abcabc";
+        String str1 = "We are students.";
+        String str2 = "aeiou";
+        System.out.println(delContainsInAnotherStr_mysolution(str1,str2));
     }
 
     /**
@@ -22,6 +24,10 @@ public class FirstNotRepeating {
      * @return
      */
     private static char firstNotRepeating_mysolution(String string){
+        if (string == null){
+            return '\0';
+        }
+
         char [] strArr = string.toCharArray();
         //Map序列有序
         Map<Character,Integer> map = new LinkedHashMap<>();
@@ -30,7 +36,6 @@ public class FirstNotRepeating {
             map.put(tempChar,map.getOrDefault(tempChar,0)+1);
         }
         //利用Lambda表达式遍历map
-        //遍历时无序
         map.forEach((key,value)-> {
             System.out.println(key+":"+value);
         });
@@ -38,12 +43,44 @@ public class FirstNotRepeating {
         System.out.println("===");
         //遍历map
         for (Map.Entry<Character,Integer> tempMap:map.entrySet()) {
-            System.out.println(tempMap.getKey()+":"+tempMap.getValue());
-//            if (tempMap.getValue()== 1){
-//                return tempMap.getKey();
-//            }
+            if (tempMap.getValue()== 1){
+                return tempMap.getKey();
+            }
+        }
+        return '\0';
+    }
+
+    /**
+     * 相关题目：1.从第一个字符串中删除在第二个字符串中出现过的所有字符。
+     * 例如：第一个字符串："We are students.",第二个字符串："aeiou"
+     * 所得结果是"W r Stdnts."
+     */
+    private static String delContainsInAnotherStr_mysolution(String str1,String str2){
+        if (str1 == null){
+            return null;
+        }
+        if (str2 == null){
+            return str1;
+        }
+        char [] str1Arr = str1.toCharArray();
+        char [] str2Arr = str2.toCharArray();
+        List<Character> list = new ArrayList<>();
+        //利用list的contains方法
+        for (char tempStr1:str1Arr) {
+            list.add(tempStr1);
+        }
+        for (char tempStr2:str2Arr) {
+            for (int i = 0; i < list.size(); i++) {
+                if (list.contains(tempStr2)) {
+                    list.remove(list.indexOf(tempStr2));
+                }
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for (char resultTemp:list){
+            result.append(resultTemp);
         }
 
-        return '\0';
+        return String.valueOf(result);
     }
 }
